9c417268251a33c58210c7f9b64c59a138383615
Lucas <lu.urzua@duocuc.cl>
2025-10-27 23:54:03 -0300

    Se integra Test de Concepto Tecnico.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from "vitest/config";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
@@ -8,6 +8,7 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/setupTests.ts",
+    include: ["src/**/*.{test,spec}.{js,ts,jsx,tsx}"],
     css: true,
   },
 });
